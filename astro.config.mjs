da8f29d8351a14841ee53831cdffadf5e610020a
Brian Brotsos <brian@brotsos.org>
2026-04-18 15:24:38 -0400

    updated deploy script from github

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://brotsoscom',
+  site: 'https://brian.brotsos.com',
 
   vite: {
     plugins: [tailwindcss()]
